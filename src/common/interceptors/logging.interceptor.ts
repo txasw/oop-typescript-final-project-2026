3da25751e80bbb131278cc9eabe75f1f45b6385a
68010244-cloud <68010244@kmitl.ac.th>
2026-03-05 02:31:56 +0700

    feat: add global request logging interceptor

---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger("HTTP");
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+    const { method, originalUrl } = request;
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const { statusCode } = response;
+        const delay = Date.now() - now;
+        this.logger.log(
+          `[${method}] ${originalUrl} -> ${statusCode} (${delay}ms)`,
+        );
+      }),
+    );
+  }
+}
