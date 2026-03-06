23759d7ff55c877861571f1f7cf8332fa265174c
izcazy <ice.izcazy@gmail.com>
2026-03-06 16:54:48 +0700

    feat: register TransactionsModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { BooksModule } from "./modules/books/books.module";
 import { MembersModule } from "./modules/members/members.module";
 import { HealthModule } from "./modules/health/health.module";
+import { TransactionsModule } from "./modules/transactions/transactions.module";
 
 @Module({
-  imports: [BooksModule, MembersModule, HealthModule],
+  imports: [BooksModule, MembersModule, HealthModule, TransactionsModule],
   controllers: [],
   providers: [],
 })
